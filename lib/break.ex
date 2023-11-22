2e8536302023732844527fe782781bab0d297488
Zach Daniel <zach@zachdaniel.dev>
2023-11-22 14:18:50 -0500

    further simplify example breakage

--- lib/break.ex
+++ lib/break.ex
@@ -3,12 +3,18 @@ defmodule Break do
   Documentation for `Break`.
   """
 
-  # def work do
-  #   AnAdopter.read()
-  # end
+  @doc """
+  Example of `break!` not working
 
+      iex(1)> break! AnAdopter.read
+      1
+      iex(2)> Break.break
+      ** (CaseClauseError) no case clause matching: {:line, :infinity}
+          (iex 1.15.4) lib/iex/pry.ex:84: IEx.Pry.pry/2
+          (iex 1.15.4) lib/iex/pry.ex:89: IEx.Pry.pry_with_next/3
+          (break 0.1.0) AnAdopter.read/0
+  """
   def break do
-    # but this does
-    Application.get_env(:break, :adopter).read()
+    AnAdopter.read()
   end
 end
